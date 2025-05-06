87e1165b8d7a76777b4fad1f88dcd9356cfe9798
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-06 17:31:00 -0500

    Input User variable script updated

--- 05-Input-variables.sh
+++ 05-Input-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Please enter your username::"
+echo "Please enter your username::" # This is the Value to be added to Variable
 
-read USERNAME
-
+read -s USERNAME # Here USERNAME is the variable name
 echo "User name entered: $USERNAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
