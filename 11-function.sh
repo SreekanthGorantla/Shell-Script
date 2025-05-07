366cecceb7a72be2efeaa7d46641d9740a194df2
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-07 13:51:17 -0500

    Functions test

--- 11-function.sh
+++ 11-function.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 
 VALIDATE(){
      if [ $1 -ne 0 ]
     then
-        echo "$2 ...FAILURE"
+        echo -e "$2 ... $R FAILURE"
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo -e "$2 ... $G SUCCESS"
     fi
 }
 
@@ -25,7 +28,7 @@ then
     dnf install mysql -y
     VALIDATE $? "Installing MySQL"
 else
-    echo "MySQL is already ... INSTALLED"
+    echo -e "MySQL is already ... $Y INSTALLED"
 fi
 
 dnf list installed git
@@ -35,5 +38,31 @@ then
     dnf install git -y
     VALIDATE $? "Installing Git"
 else
-    echo "Git is already ... INSTALLED"
+    echo -e "Git is already ... $Y INSTALLED"
 fi
+
+
+# color codes in shell script
+# Code	Color/Style
+# 0	Reset / Normal
+# 1	Bold
+# 4	Underline
+# 30	Black text
+# 31	Red text
+# 32	Green text
+# 33	Yellow text
+# 34	Blue text
+# 35	Magenta text
+# 36	Cyan text
+# 37	White text
+# 40	Black background
+# 41	Red background
+# 42	Green background
+# 43	Yellow background
+# 44	Blue background
+# 45	Magenta background
+# 46	Cyan background
+# 47	White background
+
+# To print required color to the test is \e[31m
+# Example: echo -2 "\e[31m HelloWorld" 
