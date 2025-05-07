7bef0aad2f361257f7380cd45330ab6d39ba6f93
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-06 19:37:35 -0500

    Array created for movies

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES = ("pushpa" "rrr" "devara")
+
+echo "First movie: ${MOVIE[0]}"
+echo "Second movie: ${MOVIE[1]}"
+echo "Third movie: ${MOVIE[2]}"
+
+echo "All movies are: ${MOVIE[@]}"
