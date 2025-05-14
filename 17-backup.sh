136dcba658ba5cfcd68e505ba21bff08cae98cd4
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-14 14:45:47 -0500

    backup script testing

--- 17-backup.sh
+++ 17-backup.sh
@@ -27,7 +27,7 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 USAGE(){
     #echo -e "$R USAGE:: $N sh 17-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
-    echo -e "$R USAGE:: $N sh backup <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
     exit 1
 }
 
