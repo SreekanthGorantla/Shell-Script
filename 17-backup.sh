cf5d92feadb14c1316c415083f4b6bf051e7b76d
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-14 14:48:06 -0500

    backup script testing

--- 17-backup.sh
+++ 17-backup.sh
@@ -10,7 +10,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # if user is not providing number of days we are taking 14 days as default
 
-LOGS_FOLDER="/var/log/backup-logs"
+LOGS_FOLDER="/home/ec2-user/backup-logs"
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -31,7 +31,7 @@ USAGE(){
     exit 1
 }
 
-mkdir -p $LOGS_FOLDER
+mkdir -p /home/ec2-user/backup-logs
 
 if [ $# -lt 2 ]
 then
