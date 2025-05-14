ef096602b043cf3747e203e63c871889fdcfa634
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-13 23:42:37 -0500

    delete log file test

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -35,7 +35,7 @@ CHECK_ROOT(){
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $TIMESTAMP" &>> $LOG_FILE_NAME
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14 rm -rf)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r file
