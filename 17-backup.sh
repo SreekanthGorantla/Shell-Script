06fd0958c50f578908c4fcab7291e96932420c39
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-14 11:23:41 -0500

    backup script testing

--- 17-backup.sh
+++ 17-backup.sh
@@ -54,9 +54,11 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 echo "Files are: $FILES"
 
-if [ -n "$FILES" ]
+if [ -n "$FILES" ] #True if there are files to Zip
 then
     echo "Files are: $FILES"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 else
     echo "No files found olderthan $DAYS"
 fi
