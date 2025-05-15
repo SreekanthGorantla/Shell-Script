67145499be6220d78f65339db7e011b9339dca57
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-14 20:07:31 -0500

    disk usage

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -9,10 +9,15 @@ do
     USAGE=$(echo $line | awk -F " " '{print $6F}'  | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     #echo "Partition: $PARTITION , Usage: $USAGE"
-    if [ $USAGE -gt $DISK_THRESSHOLD ]
+    if [ $USAGE -ge $DISK_THRESSHOLD ]
     then
         MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n"
     fi
 done <<< $DISK_USAGE
 
 echo -e "Message: $MSG"
+
+#echo "$MSG" | sendmail/mutt -s "High Disk Usage" <email>
+
+#Cron setup
+#PATH=/usr/bin:/bin:/usr/local/bin
