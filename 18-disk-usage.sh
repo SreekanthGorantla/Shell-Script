f135c74debb0753febbfd6e97cbd3c73a86999ab
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-14 19:46:28 -0500

    disk usage

---
+++ 18-disk-usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -h |grep xfs)
+DISK_THRESSHOLD=5
+
+while read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partition: $PARTITION, Usage: $USAGE"
+done <<< $DISK_USAGE
