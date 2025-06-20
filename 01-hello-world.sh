089ff04c8353a25a5a03c68bb9cbe86adf0af3ca
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-06-20 11:04:18 -0500

    Shell modification testing

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -6,14 +6,15 @@
 # echo "how is the world!"
 
 a=0
-while [ "$a" -lt 10 ]
+while [ "$a" -lt 10 ];
 do
     b="$a"
-    while [ "$b" -ge 0]
+    while [ "$b" -ge 0 ];
     do
         echo -n "$b "
-        b='expr $b - 1'
+        b=$(expr $b - 1)
     done
     echo
-    a='expr $a + 1'
+    a=$(expr $a + 1)
 done
+
