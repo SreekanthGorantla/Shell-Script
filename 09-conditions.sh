cc77b20dafdac4b9581dcced134a63243ce906e1
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-06-21 13:46:39 -0500

    Shell modification testing

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-NUMBER=$1
-# -gt(greater than), -lt(less than), -ge(greater than eauals), -le
-if [ $NUMBER -gt 100 ]
+# NUMBER=$1
+# # -gt(greater than), -lt(less than), -ge(greater than eauals), -le
+# if [ $NUMBER -gt 100 ]
+# then
+#     echo "Given number is greater than 100"
+# else
+#     echo "Given number is less than or equal to 100"
+# fi
+
+echo "What is today of a week? "
+read WEEK
+if [ $WEEK -ne Sunday ]
 then
-    echo "Given number is greater than 100"
+    echo "You have to go to School."
 else
-    echo "Given number is less than or equal to 100"
+    echo "Today is holiday. No School."
 fi
